2bbbd31fe1ef7ea0abad0b561d03abe4b8b603e4
Alberto Lozano <alfz@kamstrup.com>
2024-06-06 15:59:33 +0200

    Extracted SHIP_SIZE

--- src/context/InitialGameState.ts
+++ src/context/InitialGameState.ts
@@ -2,10 +2,11 @@ import { GameState } from "../types/types";
 import { generateShips } from "../utils/utils";
 
 const BOARD_SIZE = 10;
+const SHIPS_SIZES = [5, 4, 4];
 
 export const initialGameState: GameState = {
   size: BOARD_SIZE,
   hits: [],
-  ships: generateShips(BOARD_SIZE, [5, 4, 4]),
+  ships: generateShips(BOARD_SIZE, SHIPS_SIZES),
   misses: [],
 };
